e51863589a23542e7bb60bf7ed76834eba0d73c5
Peanut <123972373+prenticem@users.noreply.github.com>
2023-02-09 10:30:29 -0600

    Lab06

---
+++ JS script.js
@@ -0,0 +1,28 @@
+let a =1;
+
+function lesson(x){
+ let b = 2;
+ console.log(a);
+ console.log(b);   
+}
+
+lesson()
+
+console.log(b)
+
+
+
+
+
+
+function whodat() {
+//let name = prompt("What is your name");
+if(name != "DJ") {
+    alert("ERROR");
+} else {
+    alert("Hello")
+}
+
+
+
+}
